76a382095c72dc1b8396f377903a5cce411420ad
권순현 <ssafy5552@gmail.com>
2021-10-27 14:20:49 +0900

    feat: 회원정보수정
    회원정보수정 만들었습니다
    현재 로그인한 유저정보와 수정할려는 유저정보가 같아야 수정이 가능합니다.
    
    resolves: #S05P31D201-37

--- BE/accounts/views.py
+++ BE/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import get_user_model
 from django.http import JsonResponse
+from .serializer import GetUserSerializer, UserSerializer
 
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
@@ -8,17 +9,16 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework.response import Response
 
-from .forms import UserCreationForm
-
 @api_view(['POST'])
 def signup(request):
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-        user = form.save()
+    print(request.data)
+    serializer = UserSerializer(data = request.data)
+    if serializer.is_valid(raise_exception=True):
+        user = serializer.save()
         user.set_password(request.data.get('password'))
         user.save()
-        return Response(form.data, status=status.HTTP_201_CREATED)
-    return Response(form.errors.as_data(), status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors.as_data(), status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'PUT'])
@@ -26,16 +26,16 @@ def signup(request):
 @authentication_classes([JSONWebTokenAuthentication])
 def update(request, user_pk):
     user = get_object_or_404(get_user_model(), pk=user_pk)
-    context = {
-        "pk"          : user_pk,
-        "email"       : user.email,
-        "nickname"    : user.nickname,
-        "phone"       : user.phone,
-        "address"     : user.address,
-        "profile_img" : user.profile_img,
-        "create_at"   : user.create_at,
-        "is_seller"   : user.is_seller,
-        "id_admin"    : user.is_admin,
-    }
     if request.method == 'GET':
-        return JsonResponse(context, status=status.HTTP_200_OK)
+        serializer = GetUserSerializer(user)
+        return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+
+    if request.user == user and request.method == 'PUT':
+        serializer = UserSerializer(user, data = request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            user.set_password(request.data.get('password'))
+            user.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors.as_data(),status=status.HTTP_400_BAD_REQUEST)
+
