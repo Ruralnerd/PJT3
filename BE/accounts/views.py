6e0d2629d3ca789257edf8e5405c1df0117f7aad
권순현 <ssafy5552@gmail.com>
2021-10-27 11:31:31 +0900

    feat: 회원정보조회
    회원정보 조회를 만들었습니다.
    토큰이 없을시에 401에러
    회원이 없을시에 404에러 확인했습니다.
    
    resolves: #S05P31D201-50

--- BE/accounts/views.py
+++ BE/accounts/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import get_user_model
+from django.http import JsonResponse
 
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework.response import Response
 
 from .forms import UserCreationForm
@@ -15,4 +19,23 @@ def signup(request):
         user.save()
         return Response(form.data, status=status.HTTP_201_CREATED)
     return Response(form.errors.as_data(), status=status.HTTP_400_BAD_REQUEST)
-    
+
+
+@api_view(['GET', 'PUT'])
+@permission_classes([IsAuthenticated])
+@authentication_classes([JSONWebTokenAuthentication])
+def update(request, user_pk):
+    user = get_object_or_404(get_user_model(), pk=user_pk)
+    context = {
+        "pk"          : user_pk,
+        "email"       : user.email,
+        "nickname"    : user.nickname,
+        "phone"       : user.phone,
+        "address"     : user.address,
+        "profile_img" : user.profile_img,
+        "create_at"   : user.create_at,
+        "is_seller"   : user.is_seller,
+        "id_admin"    : user.is_admin,
+    }
+    if request.method == 'GET':
+        return JsonResponse(context, status=status.HTTP_200_OK)
