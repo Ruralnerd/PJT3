cf7f7ed3d34c85c4720b0fb7aac7dbff40d035a8
권순현 <ssafy5552@gmail.com>
2021-10-29 13:12:55 +0900

    fix : change url
    api/v1/을 추가했습니다

--- backend/final_pjt/urls.py
+++ backend/final_pjt/urls.py
@@ -40,9 +40,9 @@ urlpatterns = [
     path(DEFAULT_URL+'admin/', admin.site.urls),
 
     # Auto DRF API docs
-    path('swagger<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/v1/swagger<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('api/v1/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/v1/docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 
