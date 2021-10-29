8553c47559be145b69abb1cc5208c2ebfdc7b66f
권순현 <ssafy5552@gmail.com>
2021-10-29 11:50:40 +0900

    feat : swagger
    스웨거 설정 완료했습니다.
    signup과정에서 data입력칸이 스웨거에서 보이지않아서 데코레이터 추가했습니다.

--- backend/final_pjt/urls.py
+++ backend/final_pjt/urls.py
@@ -15,10 +15,34 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+
+from rest_framework.permissions import AllowAny 
+from drf_yasg.views import get_schema_view 
+from drf_yasg import openapi
 
 DEFAULT_URL = 'api/v1/'
+
+schema_view = get_schema_view( 
+    openapi.Info( 
+        title="Open API", 
+        default_version='v1', 
+        description="시스템 API", 
+        terms_of_service="https://www.google.com/policies/terms/", 
+    ), 
+    public=True, 
+    permission_classes=(AllowAny,), 
+)
+
 urlpatterns = [
     path(DEFAULT_URL+'accounts/', include('accounts.urls')),
     path(DEFAULT_URL+'story/', include('articles.urls')),
     path(DEFAULT_URL+'admin/', admin.site.urls),
+
+    # Auto DRF API docs
+    path('swagger<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+
