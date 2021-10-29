225405128be7d0f6520b0fd76ec7625101dae64f
권순현 <ssafy5552@gmail.com>
2021-10-29 17:37:23 +0900

    feat: 주문조회 및 삭제
    주문조회 및 삭제를 구성하였습니다.
    
    resolves : #S05P31D201-56

--- backend/sales/serializer.py
+++ backend/sales/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from accounts.serializer import UserOrderSerializer
 from .models import Request
 
 
@@ -8,3 +9,18 @@ class RequestBuyerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Request
         fields = '__all__'
+
+
+class RequestSerializer(serializers.ModelSerializer):
+    buyer = UserOrderSerializer()
+
+    class Meta:
+        model = Request
+        fields = '__all__'
+
+
+class RequestSellerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Request
+        fields = ['state', 'waybill', 'comp_date']
