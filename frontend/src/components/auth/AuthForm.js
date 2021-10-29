21a45b829db8f479fa905b1825d59f5cf283e3ed
최준원 <cjw1102@naver.com>
2021-10-29 19:41:56 +0900

    feat: Mui 폰트 커스터마이징

--- frontend/src/components/auth/AuthForm.js
+++ frontend/src/components/auth/AuthForm.js
@@ -11,6 +11,7 @@ import {
 import IconButton from '@mui/material/IconButton'
 import Visibility from '@mui/icons-material/Visibility'
 import VisibilityOff from '@mui/icons-material/VisibilityOff'
+import { ThemeProvider, createTheme } from '@mui/material'
 import Button from '../common/Button'
 import palette from '../../lib/styles/palette'
 import { useState } from 'react'
@@ -21,13 +22,10 @@ import { Link } from 'react-router-dom'
  */
 const AuthFormWrapper = css``
 
-const ButtonWithMarginTop = css`
-  margin-top: 1rem;
-`
-
 const Footer = css`
   margin-top: 1rem;
   text-align: right;
+  font-size: 0.875rem;
   a {
     color: ${palette.gray[6]};
     text-decoration: underline;
@@ -36,6 +34,12 @@ const Footer = css`
     }
   }
 `
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ['Noto Sans KR'],
+  },
+})
 
 const textMap = {
   login: '로그인',
@@ -59,53 +63,33 @@ const AuthForm = ({ type }) => {
 
   return (
     <div css={AuthFormWrapper}>
-      <Box
-        sx={{
-          display: 'grid',
-          gap: 2,
-        }}
-      >
-        <div className="formHeader">
-          {type === 'login' ? '이메일로 로그인' : '회원가입'}
-        </div>
-        <TextField
-          id="text-email"
-          name="email"
-          label="이메일"
-          variant="standard"
-          // value={state.email}
-          // onChange={onChange('email')}
-        />
-        <FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
-          <InputLabel htmlFor="text-password">비밀번호</InputLabel>
-          <Input
-            id="text-password"
-            name="password"
-            label="비밀번호"
-            type={state.showPassword ? 'text' : 'password'}
+      <ThemeProvider theme={theme}>
+        <Box
+          sx={{
+            display: 'grid',
+            gap: 2,
+          }}
+        >
+          <div className="formHeader">
+            {type === 'login' ? '이메일로 로그인' : '회원가입'}
+          </div>
+          <TextField
+            id="text-email"
+            name="email"
+            label="이메일"
             variant="standard"
-            // value={state.password}
-            // onChange={onChange('password')}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                >
-                  {state.showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
+            // value={state.email}
+            // onChange={onChange('email')}
           />
-        </FormControl>
-        {/* 회원가입일 경우 추가로 입력하는 Input */}
-        {type === 'register' && (
-          <FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
-            <InputLabel htmlFor="text-password">비밀번호 확인</InputLabel>
+          <FormControl
+            sx={{ m: 1, width: '25ch', fontFamily: 'Noto Sans KR' }}
+            variant="standard"
+          >
+            <InputLabel htmlFor="text-password">비밀번호</InputLabel>
             <Input
-              id="text-passwordConfirm"
-              name="passwordConfirm"
-              label="비밀번호 확인"
+              id="text-password"
+              name="password"
+              label="비밀번호"
               type={state.showPassword ? 'text' : 'password'}
               variant="standard"
               // value={state.password}
@@ -122,19 +106,44 @@ const AuthForm = ({ type }) => {
               }
             />
           </FormControl>
-        )}
-
-        <div css={Footer}>
-          {type === 'login' ? (
-            <Link to="/register">회원가입</Link>
-          ) : (
-            <Link to="/login">로그인</Link>
+          {/* 회원가입일 경우 추가로 입력하는 Input */}
+          {type === 'register' && (
+            <FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
+              <InputLabel htmlFor="text-password">비밀번호 확인</InputLabel>
+              <Input
+                id="text-passwordConfirm"
+                name="passwordConfirm"
+                label="비밀번호 확인"
+                type={state.showPassword ? 'text' : 'password'}
+                variant="standard"
+                // value={state.password}
+                // onChange={onChange('password')}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                    >
+                      {state.showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
           )}
-        </div>
-        <Button orange fullWidth>
-          {text}
-        </Button>
-      </Box>
+
+          <div css={Footer}>
+            {type === 'login' ? (
+              <Link to="/register">회원가입</Link>
+            ) : (
+              <Link to="/login">로그인</Link>
+            )}
+          </div>
+          <Button orange fullWidth>
+            {text}
+          </Button>
+        </Box>
+      </ThemeProvider>
     </div>
   )
 }
