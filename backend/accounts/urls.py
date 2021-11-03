f43f3d5dbf4acc0d31b203c335b09cf31396c5b2
권순현 <ssafy5552@gmail.com>
2021-11-03 17:03:22 +0900

    feat: 카카오로그인
    카카오로그인 구현중입니다.

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('users/', views.signup),
     path('login/', obtain_jwt_token),
+    path('kakaologin/', views.kakaologin),
+    path('kakaologin/callback/', views.kakaologin_callback),
     path('users/<int:user_pk>/follow/', views.follow),
     path('users/<int:user_pk>/', views.update),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render, get_object_or_404
+from django.http import response
+from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth import get_user_model
-from .serializer import GetUserSerializer, UserSerializer, UserSmallSerializer
+import requests
 
+from decouple import config
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework.response import Response
+
+from .serializer import GetUserSerializer, UserSerializer, UserSmallSerializer
+
 
 @swagger_auto_schema(method='post', request_body=UserSerializer)
 @api_view(['POST'])
@@ -20,6 +25,46 @@ def signup(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors.as_data(), status=status.HTTP_400_BAD_REQUEST)
 
+
+@api_view(['GET'])
+def kakaologin(request):
+    URL = "https://kauth.kakao.com/oauth/authorize"
+    client_id = config('KAKAO_RESTAPI')
+    redirect_uri = "http://localhost:8000/api/v1/accounts/kakaologin/callback/"
+    
+    # 인가코드 받기
+    return redirect(
+        f'{URL}?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code'
+    )
+
+
+def kakaologin_callback(request):
+    authorization_code = request.GET.get('code')
+    URL = "https://kauth.kakao.com/oauth/token"
+    headers = {
+        'Content-Type': "application/x-www-form-urlencoded;charset=utf-8"
+    }
+    params = {
+        'grant_type' : 'authorization_code',
+        'client_id' : config('KAKAO_RESTAPI'),
+        'redirect_uri': "http://localhost:8000/api/v1/accounts/kakaologin/callback/",
+        'code' : authorization_code,
+        'client_secret' : config('KAKAO_CLIENT_SECRET')
+    }
+    # 토큰 받기
+    token_res = requests.post(URL, headers= headers, params=params)
+
+    access_token = token_res.json().get('access_token')
+
+    user_data = requests.get(
+        "https://kapi.kakao.com/v2/user/me", 
+        headers={
+            'Authorization' : "Bearer " + access_token
+        })
+    
+    print(user_data.json())
+
+    return Response(user_data.json(), status=status.HTTP_200_OK)
 
 @api_view(['GET', 'PUT'])
 @permission_classes([IsAuthenticated])
