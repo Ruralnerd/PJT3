f9b826b81cd80125f29e0cec2748e95336212bb3
조여래 <whdufo@hanmail.net>
2021-11-13 14:44:13 +0900

    fix: BASE_URL edit

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -20,7 +20,7 @@ from .serializer import GetUserSerializer, UserSerializer, UserLoginSerializer
 from .models import User
 
 BASE_URL = "http://k5d201.p.ssafy.io/api/v1/"
-BASE_URL = "http://localhost:8000/api/v1/"
+# BASE_URL = "http://localhost:8000/api/v1/"
 
 @swagger_auto_schema(
     method='post',
