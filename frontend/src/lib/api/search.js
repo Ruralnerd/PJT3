3647c6a464ae2a9baeb1570448dcbe663a3d6e02
leeww0598 <leeww0598@gmail.com>
2021-11-15 14:02:46 +0900

    fix: 주석 삭제, 불필요한 코드 삭제

--- frontend/src/lib/api/search.js
+++ frontend/src/lib/api/search.js
@@ -7,12 +7,7 @@ if (ACCESS_TOKEN) {
 }
 
 export const getSearch = ({ mainKeyword, keyword }) => {
-  client.defaults.params = null
   return client.get(`/api/v1/searches/categorys/${mainKeyword}/`, {
-    // ?category_name=goguma
     params: { category_name: keyword },
   })
 }
-// export const getSearch = () => {
-//   console.log('하이 ㅋㅋ')
-// }
