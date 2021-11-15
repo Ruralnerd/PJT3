c2e7eae1db3df160c05435b9e9dc3f615a965bb8
최준원 <cjw1102@naver.com>
2021-11-15 17:20:48 +0900

    feat: 판매 글 작성, 프로필 axios 헤더 오류 해결

--- frontend/src/components/sale/SaleList.js
+++ frontend/src/components/sale/SaleList.js
@@ -61,7 +61,7 @@ const SaleList = ({ list, error, loading, auth }) => {
   return (
     <SaleListWrapper>
       <SaleListButtonWrapper>
-        {auth && (
+        {auth !== null && (
           <Button red to="/sale">
             판매 글 작성하기
           </Button>
--- frontend/src/lib/api/home.js
+++ frontend/src/lib/api/home.js
@@ -6,27 +6,22 @@ if (ACCESS_TOKEN) {
   client.defaults.headers['Authorization'] = `JWT ${ACCESS_TOKEN}`
 }
 
-export const getCarousel = () => {
-  client.defaults.params = { num: 5, option: 'manyorder' }
-  return client.get(`/api/v1/sales/markets/`)
-}
+export const getCarousel = () =>
+  client.get(`/api/v1/sales/markets/`, {
+    params: { num: 5, option: 'manyorder' },
+  })
 
-export const getPopular = () => {
-  client.defaults.params = { num: 3, option: 'popular' }
-  return client.get(`/api/v1/sales/markets/`)
-}
+export const getPopular = () =>
+  client.get(`/api/v1/sales/markets/`, {
+    params: { num: 3, option: 'popular' },
+  })
 
-export const getSeasonal = () => {
-  client.defaults.params = { num: 3, option: 'created_at' }
-  return client.get(`/api/v1/sales/markets/`)
-}
+export const getSeasonal = () =>
+  client.get(`/api/v1/sales/markets/`, {
+    params: { num: 3, option: 'created_at' },
+  })
 
-export const getStorys = () => {
-  client.defaults.params = { num: 100, option: 'popular' }
-  return client.get(`/api/v1/articles/storys/`)
-}
-
-// export const getStory = (id) => {
-//   client.defaults.params = null
-//   return client.get(`/api/v1/sales/markets/${id}`)
-// }
+export const getStorys = () =>
+  client.get(`/api/v1/articles/storys/`, {
+    params: { num: 100, option: 'popular' },
+  })
