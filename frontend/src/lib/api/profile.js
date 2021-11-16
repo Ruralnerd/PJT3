3d276e56f9382c9c6521a85ceaf3ee40e75c1355
최준원 <cjw1102@naver.com>
2021-11-16 14:59:09 +0900

    fix: 로그인 직후 401 인증에러 해결

--- frontend/src/lib/api/profile.js
+++ frontend/src/lib/api/profile.js
@@ -1,10 +1,5 @@
 import client from './client'
 
-const ACCESS_TOKEN = localStorage.getItem('token')
-if (ACCESS_TOKEN) {
-  client.defaults.headers.common['Authorization'] = `JWT ${ACCESS_TOKEN}`
-}
-
 export const getProfile = (id) => client.get(`/api/v1/accounts/users/${id}/`)
 
 export const putProfile = ({ form }) =>
