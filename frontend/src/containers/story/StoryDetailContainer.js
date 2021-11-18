9b9ec806ed9d4b6c148cf7fb73eb7aa643749eb5
최준원 <cjw1102@naver.com>
2021-11-18 11:41:23 +0900

    feat: 마켓, 스토리 글 삭제 기능
    해당 글을 작성한 사용자인 경우 삭제 가능

--- frontend/src/containers/story/StoryDetailContainer.js
+++ frontend/src/containers/story/StoryDetailContainer.js
@@ -1,14 +1,22 @@
 import { useEffect } from 'react'
 import StoryDetail from '../../components/story/StoryDetail'
 import { useDispatch, useSelector } from 'react-redux'
-import { getStory, initialize } from '../../modules/story'
+import { deleteStory, getStory, initialize } from '../../modules/story'
+import { withRouter } from 'react-router'
 
-const StoryDetailContainer = ({ match }) => {
+const StoryDetailContainer = ({ match, history }) => {
   const dispatch = useDispatch()
 
   const id = match.params.id
 
-  const { story } = useSelector(({ story }) => ({ story: story.story }))
+  const { story, user_id, error, loading } = useSelector(
+    ({ story, auth, loading }) => ({
+      story: story.story,
+      error: story.error,
+      user_id: auth.auth.id,
+      loading: loading,
+    }),
+  )
 
   useEffect(() => {
     dispatch(getStory(id))
@@ -17,11 +25,21 @@ const StoryDetailContainer = ({ match }) => {
     }
   }, [dispatch, id])
 
+  const onDeleteStory = () => {
+    dispatch(deleteStory(id))
+    alert('스토리를 삭제했습니다.')
+    history.push('/story')
+  }
+
   return (
-    <>
-      <StoryDetail story={story} />
-    </>
+    <StoryDetail
+      user_id={user_id}
+      story={story}
+      error={error}
+      loading={loading}
+      onDeleteStory={onDeleteStory}
+    />
   )
 }
 
-export default StoryDetailContainer
+export default withRouter(StoryDetailContainer)
